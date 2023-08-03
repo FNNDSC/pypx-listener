02d3c66691cafc653fbbe5bd76e3a75bcaea9da2
Jennings Zhang <jenni_zh@protonmail.com>
2023-08-02 23:47:56 -0400

    Add PatientID and SeriesInstanceUID to output

--- src/ndjson_log.rs
+++ src/ndjson_log.rs
@@ -1,25 +1,31 @@
-use crate::dicom_data::{name_of, DicomTagAndError, DicomTagError};
+use crate::dicom_data::{name_of, DicomTagAndError};
+use crate::repack::RepackOutcome;
 use camino::{Utf8Path, Utf8PathBuf};
-use dicom::object::Tag;
-use hashbrown::HashMap;
 use serde::Serialize;
 use std::os::unix::fs::MetadataExt;
 
 /// Produce a JSON string which describes the outcome of `rx-repack`.
 pub fn json_message(
     src: &Utf8Path,
-    result: &anyhow::Result<(Utf8PathBuf, Vec<DicomTagAndError>)>,
+    result: &anyhow::Result<RepackOutcome>,
 ) -> anyhow::Result<String> {
     let msg = Message::new(src, result);
     serde_json::to_string(&msg).map_err(anyhow::Error::from)
 }
 
+#[allow(non_snake_case)]
 #[derive(Serialize, Debug)]
 struct Message<'a> {
     src: &'a Utf8Path,
     dst: Option<&'a Utf8Path>,
     #[serde(skip_serializing_if = "Option::is_none")]
     size: Option<u64>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    PatientID: Option<&'a str>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    SeriesInstanceUID: Option<&'a str>,
+
     #[serde(skip_serializing_if = "Option::is_none")]
     error: Option<String>,
     #[serde(skip_serializing_if = "Vec::is_empty")]
@@ -44,21 +50,24 @@ impl From<&DicomTagAndError> for DicomTagNameAndError {
 }
 
 impl<'a> Message<'a> {
-    fn new(
-        src: &'a Utf8Path,
-        result: &'a anyhow::Result<(Utf8PathBuf, Vec<DicomTagAndError>)>,
-    ) -> Self {
+    fn new(src: &'a Utf8Path, result: &'a anyhow::Result<RepackOutcome>) -> Self {
         match result {
-            Ok((dst, missing)) => {
-                let (size, error) = fs_err::metadata(dst)
+            Ok(outcome) => {
+                let (size, error) = fs_err::metadata(&outcome.dst)
                     .map(|metadata| (Some(metadata.size()), None))
                     .unwrap_or_else(|error| (None, Some(error.to_string())));
                 Self {
                     src,
-                    dst: Some(dst),
+                    dst: Some(&outcome.dst),
                     size,
                     error,
-                    missing: missing.iter().map(DicomTagNameAndError::from).collect(),
+                    missing: outcome
+                        .missing
+                        .iter()
+                        .map(DicomTagNameAndError::from)
+                        .collect(),
+                    PatientID: Some(&outcome.PatientID),
+                    SeriesInstanceUID: Some(&outcome.SeriesInstanceUID),
                 }
             }
             Err(e) => Self {
@@ -67,6 +76,8 @@ impl<'a> Message<'a> {
                 size: None,
                 error: Some(e.to_string()),
                 missing: Vec::new(),
+                PatientID: None,
+                SeriesInstanceUID: None,
             },
         }
     }
--- src/repack.rs
+++ src/repack.rs
@@ -10,7 +10,7 @@ pub fn repack(
     data_dir: &Utf8Path,
     log_dir: Option<&Utf8Path>,
     cleanup: bool,
-) -> anyhow::Result<(Utf8PathBuf, Vec<DicomTagAndError>)> {
+) -> anyhow::Result<RepackOutcome> {
     let dcm = dicom::object::open_file(dicom_file)?;
     let common = (&dcm).try_into()?;
     let unpack = PypxPath::new(&common, data_dir);
@@ -18,12 +18,27 @@ pub fn repack(
     fs_err::create_dir_all(&unpack.dir)?;
     copy_or_mv(dicom_file, &unpack.path, cleanup)?;
 
-    if let Some(d) = log_dir {
-        let warnings = write_logs(&dcm, &common, &unpack, d)?;
-        anyhow::Ok((unpack.path, warnings))
+    let missing = if let Some(d) = log_dir {
+        write_logs(&dcm, &common, &unpack, d)?
     } else {
-        anyhow::Ok((unpack.path, Vec::new()))
-    }
+        Vec::new()
+    };
+    let outcome = RepackOutcome {
+        dst: unpack.path,
+        missing,
+        PatientID: common.PatientID.to_string(),
+        SeriesInstanceUID: common.SeriesInstanceUID,
+    };
+    anyhow::Ok(outcome)
+}
+
+/// Information about what the function [repack] did, for logging purposes.
+#[allow(non_snake_case)]
+pub struct RepackOutcome {
+    pub dst: Utf8PathBuf,
+    pub missing: Vec<DicomTagAndError>,
+    pub PatientID: String,
+    pub SeriesInstanceUID: String,
 }
 
 fn copy_or_mv<P: AsRef<Path>, Q: AsRef<Path>>(
